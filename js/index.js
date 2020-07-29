5d4fb85c1799ff51e0e5506d54b9eeb06da2cc21
TungHoang <tunghb@vng.com.vn>
2020-07-29 16:38:20 +0700

    up

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-document.getElementsByClassName("navbar-item")[0].classList.add('active')
 
+document.getElementsByClassName("navbar-item")[0].classList.add('active')
 document.getElementsByClassName("navbar-item")[0].onclick = () => {
     document.getElementsByClassName("navbar-item")[0].classList.add('active')
     document.getElementsByClassName("navbar-item")[1].classList.remove('active')
@@ -1178,7 +1178,7 @@ document.getElementById('check-form-ad').onclick = value =>{
                 first_content_preview.classList.contains('get-error') == true ? null : first_content_preview.classList.add('get-error')
                 value_check_ad = false
                 let list = checkPolicy(value_1)
-                console.log(list[0])
+                // console.log(list[0])
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
                     if($('#banned-3').text().indexOf('Sử dụng từ ngữ bị hạn chế') == 0){
@@ -1190,6 +1190,7 @@ document.getElementById('check-form-ad').onclick = value =>{
                         $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Sử dụng từ ngữ bị hạn chế: <span>"+item+"</span></p></li>" )
                     }
                 }	
+                setTimeout(FunctionHoverWord('banned-3'),500)
             }  
             // if(checkFormat2(value_1) == 1){
             //     if(isUpperCase(value_1)==true){
@@ -1272,15 +1273,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkWarning(value_1)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#warning-4').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#warning-4').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#warning-4').text().includes(item)){
                         } else {
                             document.getElementById('warning-4').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
                 }
+                setTimeout(FunctionHoverWord('warning-4'),500)
             } 
             if(value_1.match(/\s{2,}/g)){
                 first_content_preview.classList.contains('get-error') == true ? null : first_content_preview.classList.add('get-error')
@@ -1325,15 +1327,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkPolicy(value_2)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#banned-3').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#banned-3').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#banned-3').text().includes(item)){
                         } else {
                             document.getElementById('banned-3').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
                 }	
+                setTimeout(FunctionHoverWord('banned-3'),500)
             }  
             if(checkFormat2(value_2) == 1){
                 if(isUpperCase(value_2)==true){
@@ -1416,15 +1419,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkWarning(value_2)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#warning-4').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#warning-4').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#warning-4').text().includes(item)){
                         } else {
                             document.getElementById('warning-4').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
                 }
+                setTimeout(FunctionHoverWord('warning-4'),500)
             } 
             if(value_2.match(/\s{2,}/g)){
                 second_content_preview.classList.contains('get-error') == true ? null : second_content_preview.classList.add('get-error')
@@ -1469,15 +1473,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkPolicy(value_3)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#banned-3').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#banned-3').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#banned-3').text().includes(item)){
                         } else {
                             document.getElementById('banned-3').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
-                }	
+                }
+                setTimeout(FunctionHoverWord('banned-3'),500)	
             }  
             if(checkFormat2(value_3) == 1){
                 if(isUpperCase(value_3)==true){
@@ -1559,15 +1564,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkWarning(value_3)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#warning-4').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#warning-4').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#warning-4').text().includes(item)){
                         } else {
                             document.getElementById('warning-4').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
                 }
+                setTimeout(FunctionHoverWord('warning-4'),500)
             } 
             if(value_3.match(/\s{2,}/g)){
                 third_content_preview.classList.contains('get-error') == true ? null : third_content_preview.classList.add('get-error')
@@ -1612,15 +1618,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkPolicy(value_4)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#banned-3').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#banned-3').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#banned-3').text().includes(item)){
                         } else {
                             document.getElementById('banned-3').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-first .card-error-list ul").append( "<li><p id='banned-3'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
-                }	
+                }
+                setTimeout(FunctionHoverWord('banned-3'),500)	
             }  
             if(checkFormat2(value_4) == 1){
                 if(isUpperCase(value_4)==true){
@@ -1687,14 +1694,14 @@ document.getElementById('check-form-ad').onclick = value =>{
                     
                 }
             }
-            if(value_4.match(InputLinkWeb) || value_4.match(InputPhoneNumber)){
-                fourth_content_preview.classList.contains('get-error') == true ? null : fourth_content_preview.classList.add('get-error')
-                warning_card.classList.remove('is-hidden')
-                if($('#warning-3').text().indexOf('Có số điện thoại hoặc địa chỉ website') == 0){
-                } else {
-                    $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-3'>Có số điện thoại hoặc địa chỉ website</p></li>" )
-                }
-            } 
+            // if(value_4.match(InputLinkWeb) || value_4.match(InputPhoneNumber)){
+            //     fourth_content_preview.classList.contains('get-error') == true ? null : fourth_content_preview.classList.add('get-error')
+            //     warning_card.classList.remove('is-hidden')
+            //     if($('#warning-3').text().indexOf('Có số điện thoại hoặc địa chỉ website') == 0){
+            //     } else {
+            //         $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-3'>Có số điện thoại hoặc địa chỉ website</p></li>" )
+            //     }
+            // } 
             if(checkWarning(value_4).length > 0){
                 fourth_content_preview.classList.contains('get-error') == true ? null : fourth_content_preview.classList.add('get-error')
                 //value_check_ad = false
@@ -1702,15 +1709,16 @@ document.getElementById('check-form-ad').onclick = value =>{
                 let list = checkWarning(value_4)
                 for(let i=0; i<list.length;i++){
                     let item = list[i]
-                    if($('#warning-4').text().indexOf('Sử dụng từ thiếu xác thực') == 0){
+                    if($('#warning-4').text().indexOf('Phản cảm, thiếu kiểm chứng') == 0){
                         if($('#warning-4').text().includes(item)){
                         } else {
                             document.getElementById('warning-4').innerHTML += ', <span>'+item+'</span>'
                         }
                     } else {
-                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Sử dụng từ thiếu xác thực: <span>"+item+"</span></p></li>" )
+                        $("#alert-card-second .card-error-list ul").append( "<li><p id='warning-4'>Phản cảm, thiếu kiểm chứng: <span>"+item+"</span></p></li>" )
                     }
                 }
+                setTimeout(FunctionHoverWord('warning-4'),500)
             } 
             if(value_4.match(/\s{2,}/g)){
                 fourth_content_preview.classList.contains('get-error') == true ? null : fourth_content_preview.classList.add('get-error')
@@ -1861,3 +1869,48 @@ tippy('#tippy-notice-content', {
     placement: 'right-start',
 });
 
+FunctionHoverWord = (id) => {
+    let banned_error_mess = document.getElementById(id)
+    // let banned_word_focus = banned_error_mess.getElementsByTagName('SPAN')
+
+    let first_preview_focus = document.getElementById('first-preview').innerHTML.toLowerCase();
+    let second_preview_focus = document.getElementById('second-preview').innerHTML.toLowerCase();
+    let third_preview_focus = document.getElementById('third-preview').innerHTML.toLowerCase();
+    let fourth_preview_focus = document.getElementById('fourth-preview').innerHTML.toLowerCase();
+
+    $('#'+id+' span').hover(value => {
+        if(first_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = first_preview_focus.replace(value.target.innerText,'<span>'+value.target.innerText+"</span>")
+            document.getElementById('first-preview').innerHTML = temp
+        }
+        if(second_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = second_preview_focus.replace(value.target.innerText,'<span>'+value.target.innerText+"</span>")
+            document.getElementById('second-preview').innerHTML = temp
+        }
+        if(third_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = third_preview_focus.replace(value.target.innerText,'<span>'+value.target.innerText+"</span>")
+            document.getElementById('third-preview').innerHTML = temp
+        }
+        if(fourth_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = fourth_preview_focus.replace(value.target.innerText,'<span>'+value.target.innerText+"</span>")
+            document.getElementById('fourth-preview').innerHTML = temp
+        }
+    },value => {
+        if(first_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = first_preview_focus.replace('<span>'+value.target.innerText+"</span>",value.target.innerText)
+            document.getElementById('first-preview').innerHTML = temp
+        }
+        if(second_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = second_preview_focus.replace('<span>'+value.target.innerText+"</span>",value.target.innerText)
+            document.getElementById('second-preview').innerHTML = temp
+        }
+        if(third_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = third_preview_focus.replace('<span>'+value.target.innerText+"</span>",value.target.innerText)
+            document.getElementById('third-preview').innerHTML = temp
+        }
+        if(fourth_preview_focus.indexOf(value.target.innerText)>-1){
+            let temp = fourth_preview_focus.replace('<span>'+value.target.innerText+"</span>",value.target.innerText)
+            document.getElementById('fourth-preview').innerHTML = temp
+        }
+    })
+}
