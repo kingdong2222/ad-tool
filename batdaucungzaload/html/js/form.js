79f82a214d0dba419f051d008b92f88c7984a44b
TungHoang <tunghb@vng.com.vn>
2020-10-16 14:24:39 +0700

    up

--- batdaucungzaload/html/js/form.js
+++ batdaucungzaload/html/js/form.js
@@ -17,6 +17,8 @@ let empty_another_option = 'Vui lòng nhập Ngành nghề khác'
 let used_mobile = 'Số điện thoại này đã sử dụng'
 let wrong_mobile = 'Số điện thoại không hợp lệ'
 let wrong_email = 'Email không hợp lệ'
+let no_choose_major = 'Vui lòng chọn ngành nghề'
+let full_name_err = "Vui lòng nhập đủ Họ và tên"
 
 
 //email regex
@@ -57,47 +59,72 @@ $('#choose_major li').on('click', function (e) {
 });
 
 $('#submit_form').click(() => {
+    // window.location = 'welcome.html'
     validateFunc()
 })
 
 let validateFunc = () => {
-
     if (name.value) {
-        if (mobile.value) {
-            for (let i = 0; i < list_mobile.length; i++) {
-                if (list_mobile[i] === mobile.value) {
-                    alert(used_mobile)
+        if(name.value.length >= 5){
+            if (mobile.value) {
+                for (let i = 0; i < list_mobile.length; i++) {
+                    if (list_mobile[i] === mobile.value) {
+                        alert(used_mobile) 
+                        break
+                    }
                 }
-            }
-            if (mobile.value.length >= 9) {
-                if (email.value) {
-                    if (email.value.match(email_regex)) {
-                        if (business.value) {
-                            if (major.value == 'Khác') {
-                                if (another_major.value) {
-
+                if (mobile.value.length >= 9) {
+                    if (email.value) {
+                        if (email.value.match(email_regex)) {
+                            if (business.value) {
+                                if(major.value == 'Chọn ngành nghề'){
+                                    alert(no_choose_major)
                                 } else {
-                                    alert(empty_another_option)
+                                    if (major.value == 'Khác') {
+                                        if (another_major.value) {
+                                            //go to next page
+                                            window.location = 'welcome.html'
+                                            // postToGG(name.value, mobile.value, email.value, business.value, another_major.value)
+                                        } else {
+                                            alert(empty_another_option)
+                                        }
+                                    } else {
+                                        //go to next page
+                                        window.location = 'welcome.html'
+                                        // postToGG(name.value, mobile.value, email.value, business.value, major.value)
+                                    }
                                 }
                             } else {
-                                
+                                alert(emtpy_business)
                             }
                         } else {
-                            alert(emtpy_business)
+                            alert(wrong_email)
                         }
                     } else {
-                        alert(wrong_email)
+                        alert(empty_email)
                     }
                 } else {
-                    alert(empty_email)
+                    alert(wrong_mobile)
                 }
             } else {
-                alert(wrong_mobile)
+                alert(empty_mobile)
             }
         } else {
-            alert(empty_mobile)
+            alert(full_name_err)
         }
+        
     } else {
         alert(empty_name)
     }
+}
+
+let postToGG = (name, mobile, email, business, major) => {
+    $.ajax({
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSdJa39bCIvLuslxcT9D7258-MnCo6zg_C_L0zCoNZPPM3O8-g/formResponse?",
+        data: { "entry.1353488337": name, "entry.1020220624": mobile, "entry.675973294": email, "entry.197280914": business, "entry.1048902569": major },
+        type: "POST",
+        dataType: "jsonp",
+        success: function (d) { },
+        error: function (x, y, z) { }
+    });
 }
--- batdaucungzaload/html/js/questions.js
+++ batdaucungzaload/html/js/questions.js
@@ -14,21 +14,21 @@
 // });
 
 let count_right = 0
-let time = 350
+let time = 400
 
 //question 1
 $('#question_0 li').on('click', function (e) {
     let answer = e.target.innerText
-    if(answer == 'Đã biết'){
-        setTimeout(()=>{
+    if (answer == 'Đã biết') {
+        setTimeout(() => {
             $('#question_0').addClass('hidden')
             $('#question_1').removeClass('hidden')
-        },time)
+        }, time)
     } else {
-        setTimeout(()=>{
+        setTimeout(() => {
             $('#question_0').addClass('hidden')
             $('#question_2').removeClass('hidden')
-        },time)
+        }, time)
     }
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
@@ -37,10 +37,10 @@ $('#question_0 li').on('click', function (e) {
 //question 1'
 $('#question_1 li').on('click', function (e) {
     // let answer = e.target.innerText
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_1').addClass('hidden')
         $('#question_2').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -52,13 +52,13 @@ $('#question_2 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == 'ads.zalo.me'){
+    if (answer == 'ads.zalo.me') {
         count_right += 1
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_2').addClass('hidden')
         $('#question_3').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -70,13 +70,13 @@ $('#question_3 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == 'Hệ thống tự vận hành'){
+    if (answer == 'Hệ thống tự vận hành') {
         count_right += 1
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_3').addClass('hidden')
         $('#question_4').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -88,13 +88,13 @@ $('#question_4 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == 'Lượt tương tác'){
+    if (answer == 'Lượt tương tác') {
         count_right += 1
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_4').addClass('hidden')
         $('#question_5').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -106,13 +106,13 @@ $('#question_5 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == 'Tất cả doanh nghiệp/ chủ cửa hàng từ lớn đến bé'){
+    if (answer == 'Tất cả doanh nghiệp/ chủ cửa hàng từ lớn đến bé') {
         count_right += 1
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_5').addClass('hidden')
         $('#question_6').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -124,13 +124,13 @@ $('#question_6 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == 'Hơn 58 triệu'){
+    if (answer == 'Hơn 58 triệu') {
         count_right += 1
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_6').addClass('hidden')
         $('#question_7').removeClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
 });
@@ -142,14 +142,61 @@ $('#question_7 li').on('click', function (e) {
     //     count_right += 1
     // }
     let answer = e.target.innerText
-    if(answer == '1900 988 912'){
+    if (answer == '1900 988 912') {
         count_right += 1
     }
     console.log(count_right)
-    setTimeout(()=>{
+    setTimeout(() => {
         $('#question_7').addClass('hidden')
         // $('#question_6').toggleClass('hidden')
-    },time)
+    }, time)
     $(this).parent().addClass('is-result')
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
+
+    setTimeout(() => {
+        $('.module-question').addClass('hidden')
+
+        if (count_right >= 5) {
+            //get voucher
+            $('#voucher').removeClass('hidden')
+            $('#note-content').removeClass('hidden')
+            setTimeout(() => {
+                $('#voucher').addClass('is-opacity')
+            }, 100)
+        } else {
+            //get random 4 gifts
+            let random = Math.floor(Math.random() * 3)
+            switch (random) {
+                case 0:
+                    $('#tshirt').removeClass('hidden')
+                    // $('#tshirt').addClass('is-opacity')
+                    setTimeout(() => {
+                        $('#tshirt').addClass('is-opacity')
+                    }, 100)
+                    break;
+                case 1:
+                    $('#pen').removeClass('hidden')
+                    // $('#pen').addClass('is-opacity')
+                    setTimeout(() => {
+                        $('#pen').addClass('is-opacity')
+                    }, 100)
+                    break;
+                case 2:
+                    $('#notebook').removeClass('hidden')
+                    // $('#notebook').addClass('is-opacity')
+                    setTimeout(() => {
+                        $('#notebook').addClass('is-opacity')
+                    }, 100)
+                    break;
+                case 3:
+                    $('#glasswater').removeClass('hidden')
+                    // $('#glasswater').addClass('is-opacity')
+                    setTimeout(() => {
+                        $('#glasswater').addClass('is-opacity')
+                    }, 100)
+                    break;
+            }
+        }
+    }, time)
+
 });
