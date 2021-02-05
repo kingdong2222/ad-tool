67c4e3a09d46f0a93756238b7355e966d8dc8728
TungHoang <tunghb@vng.com.vn>
2021-02-05 10:44:21 +0700

    up

--- xone_landing/html/js/register.js
+++ xone_landing/html/js/register.js
@@ -2157,3 +2157,19 @@ window.onscroll = function () {
   $(document).on("click", ".dropdown-touch", function (e) {
     e.stopPropagation();
   });
+
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("uniSearch");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("university-menu");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
